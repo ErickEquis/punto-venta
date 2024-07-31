36ce55dcd3d41560d5f5894ec8cd3a2e84dc4dd0
ErickEquis <ericksantillan95.01@gmail.com>
2024-07-31 14:21:04 -0600

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // endpoint: "https://erickdev.duckdns.org/api/"
-  endpoint: "http://localhost:8880/api/"
+  endpoint: "http://backend-prod:8880/api/"
 };
