62c95dfccd71af1328ede64000e56cdbe1a96748
ErickEquis <ericksantillan95.01@gmail.com>
2024-10-03 18:25:58 -0600

    fix: environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: "https://erickdev.duckdns.org/api/"
+  // endpoint: "https://erickdev.duckdns.org/api/",
+  endpoint: "http://backend-prod/api/"
 };
