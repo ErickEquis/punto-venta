cfc7a2af1053d20ea7b9d5b2ac4c1b8414cac0bf
ErickEquis <ericksantillan95.01@gmail.com>
2024-11-30 17:28:40 -0600

    add: modulo auth

--- frontend/src/app/auth/components/carousel/carousel.component.ts
+++ frontend/src/app/auth/components/carousel/carousel.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  host: {
+    'class': 'carousel slide h-100 d-flex align-items-center',
+    '[attr.data-bs-ride]': '"carousel"',
+    '[id]': '"carouselExampleAutoplaying"',
+  }
 })
 export class CarouselComponent implements OnInit {
 
--- frontend/src/app/auth/pages/layout-auth-page/layout-auth-page.component.ts
+++ frontend/src/app/auth/pages/layout-auth-page/layout-auth-page.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout-auth-page',
   templateUrl: './layout-auth-page.component.html',
-  styleUrls: ['./layout-auth-page.component.css']
-})
-export class LayoutAuthPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  styleUrls: ['./layout-auth-page.component.css'],
+  host: {
+    'class': 'vh-100 d-flex justify-content-center align-items-center m-0 p-0',
   }
-
-}
+})
+export class LayoutAuthPageComponent { }
